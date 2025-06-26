c2fa9cc542621306b2671c57d76e9eed1072a5c8
DilkhushGiriGoswami <127507429+DilkhushGiriGoswami@users.noreply.github.com>
2025-06-26 15:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ if st.button("Predict Salary"):
     input_data = np.array([[years_exp]])
     input_scaled=scaler.transform(input_data)
     predicted_salary = model.predict(input_scaled)
-    st.success(f"Predicted Salary: Rs. {round(float(predicted_salary[0]), 2)}")
+    st.success(f"Predicted Salary: Rs. {predicted_salary[0]}")
